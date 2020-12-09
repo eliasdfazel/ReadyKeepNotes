c5dce32a5e859c0137a4186383fd32b7c234fccf
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-12-09 10:45:55 -0900

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- KeepNotePhone/src/main/java/net/geeksempire/keepnote/EntryConfigurations.kt
+++ KeepNotePhone/src/main/java/net/geeksempire/keepnote/EntryConfigurations.kt
@@ -2,10 +2,19 @@ package net.geeksempire.keepnote
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import net.geeksempire.keepnote.databinding.EntryConfigurationLayoutBinding
 
 class EntryConfigurations : AppCompatActivity() {
+
+    lateinit var entryConfigurationLayoutBinding: EntryConfigurationLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        entryConfigurationLayoutBinding = EntryConfigurationLayoutBinding.inflate(layoutInflater)
+        setContentView(entryConfigurationLayoutBinding.root)
+
+
+
     }
+
 }
