4d6c40b4bbc4f82b89086bb139ec91aa47035a43
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-12-10 06:57:19 -0900

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- KeepNotePhone/src/main/java/net/geeksempire/keepnote/EntryConfigurations.kt
+++ KeepNotePhone/src/main/java/net/geeksempire/keepnote/EntryConfigurations.kt
@@ -2,9 +2,15 @@ package net.geeksempire.keepnote
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import net.geeksempire.keepnote.databinding.EntryConfigurationLayoutBinding
 
 class EntryConfigurations : AppCompatActivity() {
+
+    val firebaseAuth: FirebaseAuth = Firebase.auth
+    val firebaseUser = firebaseAuth.currentUser
 
     lateinit var entryConfigurationLayoutBinding: EntryConfigurationLayoutBinding
 
