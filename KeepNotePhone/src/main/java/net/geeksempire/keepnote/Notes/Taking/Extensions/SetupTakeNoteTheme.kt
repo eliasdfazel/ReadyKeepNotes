077b8589fdfc096aa015f253ee6ae263ef172a0a
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-12-11 07:03:36 -0900

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- KeepNotePhone/src/main/java/net/geeksempire/keepnote/Notes/Taking/Extensions/SetupTakeNoteTheme.kt
+++ KeepNotePhone/src/main/java/net/geeksempire/keepnote/Notes/Taking/Extensions/SetupTakeNoteTheme.kt
@@ -1,8 +1,10 @@
 package net.geeksempire.keepnote.Notes.Taking.Extensions
 
+import android.view.inputmethod.InputMethodManager
 import net.geeksempire.keepnote.Notes.Taking.TakeNote
 import net.geeksempire.keepnote.Notes.UI.PaintingCanvasView
 import net.geeksempire.keepnote.R
+import net.geeksempire.keepnote.Utils.UI.Display.DpToInteger
 
 fun TakeNote.setupTakeNoteTheme() {
 
@@ -35,5 +37,55 @@ fun TakeNote.setupTakeNoteTheme() {
 
     }
 
+}
+
+fun TakeNote.setupToggleKeyboardHandwriting() {
+
+    toggleKeyboardHandwriting = false
+
+    takeNoteLayoutBinding.toggleKeyboardHandwriting.icon = getDrawable(R.drawable.icon_keyboard)
+    takeNoteLayoutBinding.toggleKeyboardHandwriting.iconSize = DpToInteger(applicationContext, 103)
+
+    inputMethodManager.showSoftInput(
+        takeNoteLayoutBinding.editTextView,
+        InputMethodManager.SHOW_IMPLICIT
+    )
+
+    takeNoteLayoutBinding.editTextView.requestFocus()
+
+    takeNoteLayoutBinding.toggleKeyboardHandwriting.setOnClickListener {
+
+
+        if (toggleKeyboardHandwriting) {
+
+            toggleKeyboardHandwriting = false
+
+            takeNoteLayoutBinding.toggleKeyboardHandwriting.icon = getDrawable(R.drawable.icon_keyboard)
+            takeNoteLayoutBinding.toggleKeyboardHandwriting.iconSize = DpToInteger(applicationContext, 103)
+
+            inputMethodManager.showSoftInput(
+                takeNoteLayoutBinding.editTextView,
+                InputMethodManager.SHOW_IMPLICIT
+            )
+
+            takeNoteLayoutBinding.editTextView.requestFocus()
+
+        } else {
+
+            toggleKeyboardHandwriting = true
+
+            takeNoteLayoutBinding.toggleKeyboardHandwriting.icon = getDrawable(R.drawable.icon_handwriting)
+            takeNoteLayoutBinding.toggleKeyboardHandwriting.iconSize = DpToInteger(applicationContext, 71)
+
+            inputMethodManager.hideSoftInputFromWindow(
+                takeNoteLayoutBinding.editTextView.windowToken,
+                InputMethodManager.HIDE_NOT_ALWAYS
+            )
+
+            takeNoteLayoutBinding.editTextView.clearFocus()
+
+        }
+
+    }
 
 }
--- KeepNotePhone/src/main/java/net/geeksempire/keepnote/Notes/Taking/TakeNote.kt
+++ KeepNotePhone/src/main/java/net/geeksempire/keepnote/Notes/Taking/TakeNote.kt
@@ -1,16 +1,28 @@
 package net.geeksempire.keepnote.Notes.Taking
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import net.geeksempire.keepnote.Notes.Taking.Extensions.setupTakeNoteTheme
+import net.geeksempire.keepnote.Notes.Taking.Extensions.setupToggleKeyboardHandwriting
 import net.geeksempire.keepnote.Preferences.Theme.ThemePreferences
 import net.geeksempire.keepnote.databinding.TakeNoteLayoutBinding
 
 class TakeNote : AppCompatActivity() {
 
+    val inputMethodManager: InputMethodManager by lazy {
+        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
+
     val themePreferences: ThemePreferences by lazy {
         ThemePreferences(applicationContext)
     }
+
+    /**
+     * True: Handwriting - False: Keyboard
+     * */
+    var toggleKeyboardHandwriting: Boolean = false
 
     lateinit var takeNoteLayoutBinding: TakeNoteLayoutBinding
 
@@ -20,6 +32,10 @@ class TakeNote : AppCompatActivity() {
         setContentView(takeNoteLayoutBinding.root)
 
         setupTakeNoteTheme()
+
+        setupToggleKeyboardHandwriting()
+
+
 
     }
 
