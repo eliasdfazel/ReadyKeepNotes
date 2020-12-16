20713659cf5273bb89b829304af7718ddeac6bc0
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-12-16 10:27:20 -0900

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- KeepNotesPhone/src/main/java/net/geeksempire/keepnotes/Overview/UserInterface/KeepNoteOverview.kt
+++ KeepNotesPhone/src/main/java/net/geeksempire/keepnotes/Overview/UserInterface/KeepNoteOverview.kt
@@ -2,9 +2,15 @@ package net.geeksempire.keepnotes.Overview.UserInterface
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import net.geeksempire.keepnotes.Database.IO.NotesIO
+import net.geeksempire.keepnotes.KeepNoteApplication
 import net.geeksempire.keepnotes.databinding.OverviewLayoutBinding
 
 class KeepNoteOverview : AppCompatActivity() {
+
+    val notesIO: NotesIO by lazy {
+        NotesIO(application as KeepNoteApplication)
+    }
 
     lateinit var overviewLayoutBinding: OverviewLayoutBinding
 
