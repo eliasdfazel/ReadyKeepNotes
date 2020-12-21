91e6c7b143a7555df8fc7f6589e7c9b7aacd806f
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-12-21 09:20:10 -0900

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- KeepNotesPhone/src/main/java/net/geeksempire/keepnotes/Database/IO/NotesIO.kt
+++ KeepNotesPhone/src/main/java/net/geeksempire/keepnotes/Database/IO/NotesIO.kt
@@ -46,13 +46,13 @@ class NotesIO (private val keepNoteApplication: KeepNoteApplication) {
 
             val databasePath = databaseEndpoints.generalEndpoints(firebaseUser.uid) + "/" + "${documentId}"
 
+            /* Save Notes & Snapshot Of Handwriting */
             (keepNoteApplication).firestoreDatabase
                 .document(databasePath)
                 .set(notesDataStructure)
                 .addOnSuccessListener {
                     Log.d(this@NotesIO.javaClass.simpleName, "Note Saved Successfully")
 
-                    /* Save Notes & Snapshot Of Handwriting */
                     (keepNoteApplication).firebaseStorage
                         .getReference(databaseEndpoints.generalEndpoints(firebaseUser.uid) + "/${documentId}.PNG")
                         .putBytes(paintingIO.takeScreenshot(paintingCanvasView))
@@ -94,43 +94,6 @@ class NotesIO (private val keepNoteApplication: KeepNoteApplication) {
 
                         }
 
-                    /* Save Text Content Archive */
-                    (keepNoteApplication).firestoreDatabase
-                        .collection(databaseEndpoints.noteTextsEndpoints(databasePath))
-                        .add(hashMapOf(
-                            "noteTile" to notesDataStructure.noteTile,
-                            "noteTextContent" to notesDataStructure.noteTextContent
-                        ))
-                        .addOnSuccessListener {
-
-
-
-                        }.addOnFailureListener {
-
-
-
-                        }
-
-                    /* Save Paths Of Handwriting Notes */
-                    paintingCanvasView.allRedrawPaintingData.forEach {
-
-                        (keepNoteApplication).firestoreDatabase
-                            .collection(databaseEndpoints.paintPathsEndpoints(databasePath))
-                            .add(hashMapOf(
-                                "paintPath" to it.toString()
-                            ))
-                            .addOnSuccessListener {
-
-
-
-                            }.addOnFailureListener {
-
-
-
-                            }
-
-                    }
-
                 }.addOnFailureListener {
                     Log.d(this@NotesIO.javaClass.simpleName, "Note Did Note Saved")
 
@@ -161,6 +124,43 @@ class NotesIO (private val keepNoteApplication: KeepNoteApplication) {
                     )
 
                 }
+
+            /* Save Text Content Archive */
+            (keepNoteApplication).firestoreDatabase
+                .collection(databaseEndpoints.noteTextsEndpoints(databasePath))
+                .add(hashMapOf(
+                    "noteTile" to notesDataStructure.noteTile,
+                    "noteTextContent" to notesDataStructure.noteTextContent
+                ))
+                .addOnSuccessListener {
+
+
+
+                }.addOnFailureListener {
+
+
+
+                }
+
+            /* Save Paths Of Handwriting Notes */
+            paintingCanvasView.allRedrawPaintingData.forEach {
+
+                (keepNoteApplication).firestoreDatabase
+                    .collection(databaseEndpoints.paintPathsEndpoints(databasePath))
+                    .add(hashMapOf(
+                        "paintPath" to it.toString()
+                    ))
+                    .addOnSuccessListener {
+
+
+
+                    }.addOnFailureListener {
+
+
+
+                    }
+
+            }
 
         }
 
