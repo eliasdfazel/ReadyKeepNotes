cd0c650284ba0c7393025b961407bfcde0c1cb35
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-12-21 07:29:12 -0900

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- KeepNotesPhone/src/main/java/net/geeksempire/keepnotes/Database/GeneralEndpoints/DatabaseEndpoints.kt
+++ KeepNotesPhone/src/main/java/net/geeksempire/keepnotes/Database/GeneralEndpoints/DatabaseEndpoints.kt
@@ -1,10 +1,15 @@
 package net.geeksempire.keepnotes.Database.GeneralEndpoints
 
-class DatabaseEndpoints() {
+class DatabaseEndpoints {
 
     fun GeneralEndpoints (firebaseUserUniqueId: String) : String  {
 
         return "/KeepNotes/${firebaseUserUniqueId}/Notes"
+    }
+
+    fun PaintPathEndpoints (firebaseUserUniqueId: String) : String {
+
+        return GeneralEndpoints(firebaseUserUniqueId).plus("/PaintPaths")
     }
 
 }
