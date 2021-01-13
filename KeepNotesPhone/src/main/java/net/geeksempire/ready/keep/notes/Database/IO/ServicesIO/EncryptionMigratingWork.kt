75009fb7b2ad2aaae1e183f0db56d5c138f8ff29
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-01-12 18:16:27 -0900

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- KeepNotesPhone/src/main/java/net/geeksempire/ready/keep/notes/Database/IO/ServicesIO/EncryptionMigratingWork.kt
+++ KeepNotesPhone/src/main/java/net/geeksempire/ready/keep/notes/Database/IO/ServicesIO/EncryptionMigratingWork.kt
@@ -1,14 +1,21 @@
 package net.geeksempire.ready.keep.notes.Database.IO.ServicesIO
 
+import android.app.*
 import android.content.Context
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.work.CoroutineWorker
 import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
 import net.geeksempire.ready.keep.notes.AccountManager.Utils.UserInformationIO
 import net.geeksempire.ready.keep.notes.Database.Configurations.Offline.NotesRoomDatabaseConfiguration
+import net.geeksempire.ready.keep.notes.Database.DataStructure.NotesDatabaseModel
+import net.geeksempire.ready.keep.notes.R
+import net.geeksempire.ready.keep.notes.Utils.Security.Encryption.ContentEncryption
 
 class EncryptionMigratingWork(val appContext: Context, val workerParams: WorkerParameters) : CoroutineWorker(appContext, workerParams) {
+
+    val contentEncryption = ContentEncryption()
 
     object Foreground {
         const val NotificationId = 123
@@ -20,22 +27,72 @@ class EncryptionMigratingWork(val appContext: Context, val workerParams: WorkerP
 
         setForegroundAsync(ForegroundInfo(
             EncryptionMigratingWork.Foreground.NotificationId,
-            NotificationCompat.Builder(applicationContext, this@EncryptionMigratingWork.javaClass.simpleName).apply {
-                setNotificationSilent()
-            }.build()
+            createNotification()
         ))
 
         val userInformationIO = UserInformationIO(applicationContext)
 
-
+        val oldEncryptionPassword = userInformationIO.getOldFirebaseUniqueIdentifier()
+        val newEncryptionPassword = userInformationIO.getNewFirebaseUniqueIdentifier()
 
         val notesRoomDatabaseConfiguration: NotesRoomDatabaseConfiguration = NotesRoomDatabaseConfiguration(applicationContext)
 
+        val notesDatabaseDataAccessObject = notesRoomDatabaseConfiguration.prepareRead()
 
+        val allOldEncryptedData = notesDatabaseDataAccessObject.getAllNotesData()
+
+        allOldEncryptedData.forEach {
+
+            val noteTitle = it.noteTile?.let { title -> contentEncryption.decryptEncodedData(title, oldEncryptionPassword) }
+            val noteTextContent = it.noteTextContent?.let { content -> contentEncryption.decryptEncodedData(content, oldEncryptionPassword) }
+
+            notesDatabaseDataAccessObject.updateNoteData(NotesDatabaseModel(
+                it.uniqueNoteId,
+                contentEncryption.encryptEncodedData(noteTitle.orEmpty(), newEncryptionPassword).asList().toString(),
+                contentEncryption.encryptEncodedData(noteTextContent.orEmpty(), newEncryptionPassword).asList().toString(),
+                it.noteHandwritingPaintingPaths,
+                it.noteHandwritingSnapshotLink,
+                it.noteVoiceContent,
+                it.noteImageContent,
+                it.noteGifContent,
+                it.noteTakenTime,
+                it.noteEditTime,
+                it.noteIndex,
+                it.noteTags,
+                it.noteHashTags,
+                it.noteTranscribeTags,
+                it.dataSelected
+            ))
+
+        }
 
         workerResult = Result.success()
 
         return workerResult
+    }
+
+    private fun createNotification() : Notification {
+
+        val notificationManager = applicationContext.getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notificationBuilder = NotificationCompat.Builder(applicationContext, this@EncryptionMigratingWork.javaClass.simpleName)
+        notificationBuilder.setContentTitle(applicationContext.getString(R.string.applicationName))
+        notificationBuilder.setContentText(applicationContext.getString(R.string.migratingDatabaseEncryption))
+        notificationBuilder.setSmallIcon(R.drawable.lock)
+        notificationBuilder.color = applicationContext.getColor(R.color.cyan)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val notificationChannel = NotificationChannel(
+                applicationContext.packageName,
+                applicationContext.getString(R.string.applicationName),
+                NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+            notificationBuilder.setChannelId(applicationContext.packageName)
+
+        }
+
+        return notificationBuilder.build()
     }
 
 }
