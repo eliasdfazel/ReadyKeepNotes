55622340a1234c121a818a687503993a8b32c1eb
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-01-28 09:19:08 -0900

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,9 +1,13 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+});
+
+const runtimeOptions = {
+    timeoutSeconds: 313,
+}
+
